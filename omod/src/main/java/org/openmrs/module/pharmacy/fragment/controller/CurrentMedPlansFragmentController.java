1f069698fbc13d152f4abd7b685dadf43d58cf97
HariniParth <parth.harini@gmail.com>
2016-10-13 16:22:13 -0700

    Record action on Med Plans

--- omod/src/main/java/org/openmrs/module/pharmacy/fragment/controller/CurrentMedPlansFragmentController.java
+++ omod/src/main/java/org/openmrs/module/pharmacy/fragment/controller/CurrentMedPlansFragmentController.java
@@ -10,14 +10,16 @@ import java.util.HashMap;
 import java.util.List;
 import org.openmrs.DrugOrder;
 import org.openmrs.Patient;
-import java.lang.Integer;
+import java.util.Date;
 import org.openmrs.Concept;
+import org.openmrs.Order;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.drugorders.api.drugordersService;
 import org.openmrs.module.drugorders.api.drugordersdiseasesService;
 import org.openmrs.module.drugorders.drugorders;
 import org.openmrs.module.drugorders.drugordersdiseases;
 import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.util.StringUtils;
 
 /**
  *
@@ -35,6 +37,11 @@ public class CurrentMedPlansFragmentController {
         
         HashMap<Integer,drugorders> patientPlanOrdersExtn = new HashMap<Integer,drugorders>();
         
+        HashMap<Integer,Date> patientDOB = new HashMap<Integer,Date>();
+        HashMap<Integer,String> patientAddress = new HashMap<Integer,String>();
+        HashMap<Integer,String> patientIdentifiers = new HashMap<Integer,String>();
+        HashMap<Integer,String> providerIdentifiers = new HashMap<Integer,String>();
+        
         for(Patient patient : patients){
             
             List<drugordersdiseases> plans = Context.getService(drugordersdiseasesService.class).getDrugOrdersByPatient(Integer.toString(patient.getPatientId()));
@@ -49,10 +56,10 @@ public class CurrentMedPlansFragmentController {
                 patientPlans.put(patient.getGivenName()+" "+patient.getFamilyName(), planNames);
             }
             
-            HashMap<String,List<DrugOrder>> mainOrder = new HashMap<String,List<DrugOrder>>();
-            HashMap<String,List<drugorders>> extnOrder = new HashMap<String,List<drugorders>>();
-        
             if(planNames.size() > 0){
+                
+                HashMap<String,List<DrugOrder>> mainOrder = new HashMap<String,List<DrugOrder>>();
+                
                 for(String planName : planNames) {
                     Concept planConcept = Context.getConceptService().getConceptByName(planName);
                     List<drugordersdiseases> planOrders = Context.getService(drugordersdiseasesService.class).getDrugOrdersByDiseaseAndPatient(planConcept, Integer.toString(patient.getPatientId()));
@@ -65,21 +72,36 @@ public class CurrentMedPlansFragmentController {
                     List<DrugOrder> orderMain = new ArrayList<DrugOrder>();
                     
                     for(Integer order: orders){
-                        DrugOrder dOrderMain = (DrugOrder) Context.getOrderService().getOrder(order);
-                        orderMain.add(dOrderMain);
-                        
                         drugorders dOrderExtn = Context.getService(drugordersService.class).getDrugOrderByOrderID(order);
-                        patientPlanOrdersExtn.put(dOrderExtn.getOrderId(), dOrderExtn);
+                        
+                        if(dOrderExtn.getOrderstatus().equals("Active-Plan")){
+                            patientPlanOrdersExtn.put(dOrderExtn.getOrderId(), dOrderExtn);
+                            
+                            DrugOrder dOrderMain = (DrugOrder) Context.getOrderService().getOrder(order);
+                            orderMain.add(dOrderMain);
+
+                            Order o = Context.getOrderService().getOrder(order);
+                            providerIdentifiers.put(o.getOrderId(), o.getOrderer().getPerson().getGivenName() + " " + o.getOrderer().getPerson().getFamilyName() + ", " + StringUtils.capitalize(o.getOrderer().getIdentifier()));
+                        }
                     }
                     
                     mainOrder.put(planName, orderMain);
-                    
                     patientPlanOrdersMain.put(patient.getGivenName()+" "+patient.getFamilyName(), mainOrder);
                 }
+                
+                patientDOB.put(patient.getPatientId(),patient.getBirthdate());
+                patientAddress.put(patient.getPatientId(), patient.getPersonAddress().getAddress1()+" "+patient.getPersonAddress().getCityVillage()+" "+patient.getPersonAddress().getStateProvince()+"Zipcode: "+patient.getPersonAddress().getPostalCode()+" "+patient.getPersonAddress().getCountry());
+                patientIdentifiers.put(patient.getPatientId(), patient.getPatientIdentifier().toString());
+                
             }
         }
+        
         model.addAttribute("patientPlans", patientPlans);
         model.addAttribute("patientPlanOrdersMain", patientPlanOrdersMain);
         model.addAttribute("patientPlanOrdersExtn", patientPlanOrdersExtn);
+        model.addAttribute("patientDOB", patientDOB);
+        model.addAttribute("patientAddress", patientAddress);
+        model.addAttribute("patientIdentifiers", patientIdentifiers);
+        model.addAttribute("providerIdentifiers", providerIdentifiers);
     }
 }
