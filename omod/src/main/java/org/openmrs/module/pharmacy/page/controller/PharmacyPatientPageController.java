e18725ae4b2562cb6a571e272658f1945c4baae6
HariniParth <parth.harini@gmail.com>
2016-12-15 23:57:37 -0800

    Added new landing page for Pharmacy that will list Pharmacy Patients

---
+++ omod/src/main/java/org/openmrs/module/pharmacy/page/controller/PharmacyPatientPageController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openmrs.module.pharmacy.page.controller;
+
+/**
+ *
+ * @author harini-geek
+ */
+public class PharmacyPatientPageController {
+    
+    public void controller(){
+        
+    }
+}
